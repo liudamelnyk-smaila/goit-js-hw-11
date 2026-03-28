fb28f88f040af44819f2a74d8bfd1f5331ba90b1
liudamelnyk-smaila <liudamelnyk@gmail.com>
2026-03-28 22:14:43 +0200

    modificate

---
+++ src/js/render-functions.js
@@ -0,0 +1,69 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader-wrapper');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+            />
+            <div class="gallery-info">
+              <p class="info-item">
+                <span class="info-label">Likes</span>
+                <span class="info-value">${likes}</span>
+              </p>
+              <p class="info-item">
+                <span class="info-label">Views</span>
+                <span class="info-value">${views}</span>
+              </p>
+              <p class="info-item">
+                <span class="info-label">Comments</span>
+                <span class="info-value">${comments}</span>
+              </p>
+              <p class="info-item">
+                <span class="info-label">Downloads</span>
+                <span class="info-value">${downloads}</span>
+              </p>
+            </div>
+          </a>
+        </li>
+      `
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  loader.classList.add('is-hidden');
+}
